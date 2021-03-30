7b501a2f36384d2b81c54fbdd4ebdf5aa7583ada
sorenmarquardt <soren8620@hotmail.com>
2021-03-30 22:41:46 +0200

    Tilføjet mulighed for at blive tattoveret

---
+++ server-data/resources/[scripts]/[Tattoo]/hsrp_dlctattoos/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+files {
+  'mpheist3/shop_tattoo.meta',
+  'mpheist3/mpheist3_overlays.xml',
+  'mpvinewood/shop_tattoo.meta',
+  'mpvinewood/mpvinewood_overlays.xml',
+}
+
+data_file 'TATTOO_SHOP_DLC_FILE' 'mpheist3/shop_tattoo.meta'
+data_file 'PED_OVERLAY_FILE' 'mpheist3/mpheist3_overlays.xml'
+data_file 'TATTOO_SHOP_DLC_FILE' 'mpvinewood/shop_tattoo.meta'
+data_file 'PED_OVERLAY_FILE' 'mpvinewood/mpvinewood_overlays.xml'
