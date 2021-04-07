d326f1e73bd2f87b99003765e2c4ca1b519826b0
sorenmarquardt <soren8620@hotmail.com>
2021-04-07 12:01:55 +0200

    Fix

--- server-data/resources/[mar]/mar_cleararea/client.lua
+++ server-data/resources/[mar]/mar_cleararea/client.lua
@@ -2,7 +2,7 @@ Citizen.CreateThread(function()
     while (true) do
 					  --  (X, Y, Z, radius, Nix pille) 
 		ClearAreaOfPeds(440.84, -983.14, 30.69, 400.0, 1), ----PD
-		ClearAreaOfPeds(-691.52, 5772.38, 17.33, 400.0, 1) --Paleto moteller
+		ClearAreaOfPeds(-691.52, 5772.38, 17.33, 400.0, 1), --Paleto moteller
 		ClearAreaOfPeds(324.78, -212.54, 54.09, 400.0, 1) --PinkCage
         Citizen.Wait(0)
     end
